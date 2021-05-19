e07b1f0929ba29bb9f9bb5358044bdc76d51ec12
hemanthkoppadi <hemanthnarasimhavarma9@gmail.com>
2021-05-19 11:42:38 +0530

    Initial commit

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-
+const dotenv = require('dotenv').config();
+const api_key = process.env.API_KEY;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -18,7 +19,7 @@ app.post("/", function(req, res) {
 
 
     const query = req.body.cityName;
-    const api_key = "2b5a6b60a349c7f46631b5e11aeac026";
+
     const units = "metric";
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + api_key + "&units=" + units;
     https.get(url, function(response) {
@@ -27,7 +28,7 @@ app.post("/", function(req, res) {
         response.on("data", function(data) {
             const weatherData = JSON.parse(data);
             if (response.statusCode === 200) {
-                const temp = "And the Temperature is " + weatherData.main.temp;
+                const temp = "And the Temperature is " + weatherData.main.temp + " celsius";
                 const Description = "In " + query + " it's " + weatherData.weather[0].description;
                 const icon = weatherData.weather[0].icon;
                 const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
